3a4e74330d03c57682976b560445ac9545c1313b
Marcio Camurati <marcio.camurati@gmail.com>
2017-09-19 21:41:28 -0300

    implement simple log info request and response masking card informations to help Cielo debug

--- api30.sdk/src/main/java/cieloecommerce/sdk/ecommerce/request/AbstractSaleRequest.java
+++ api30.sdk/src/main/java/cieloecommerce/sdk/ecommerce/request/AbstractSaleRequest.java
@@ -3,10 +3,14 @@ package cieloecommerce.sdk.ecommerce.request;
 import cieloecommerce.sdk.Environment;
 import cieloecommerce.sdk.Merchant;
 import com.google.gson.Gson;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestWrapper;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClientBuilder;
 
@@ -15,6 +19,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.zip.GZIPInputStream;
 
 /**
@@ -25,6 +33,21 @@ public abstract class AbstractSaleRequest<Request, Response> {
 	final Environment environment;
 	private final Merchant merchant;
 	private HttpClient httpClient;
+
+	/*
+	 * RegExp patterns to PCI log informations
+	 */
+	private final Pattern patternCreditCardNumber = Pattern.compile("(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|" +
+			"6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|" +
+			"[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})");
+
+	private final Pattern patternSecurityCode = Pattern.compile("\\\"(\\d{3}|\\d{4})\\\"");
+
+	private final Pattern patternValidateDate = Pattern.compile("\\\"\\d{1,2}/\\d{4}\\\"");
+
+	private final Pattern patternMaskedCreditCardNumber = Pattern.compile("\\d{6}\\*+\\d{4}");
+
+	private static final Logger LOG = Logger.getLogger(AbstractSaleRequest.class.getName());
 
 	AbstractSaleRequest(Merchant merchant, Environment environment) {
 		this.merchant = merchant;
@@ -60,6 +83,30 @@ public abstract class AbstractSaleRequest<Request, Response> {
 		request.addHeader("MerchantKey", merchant.getKey());
 		request.addHeader("RequestId", UUID.randomUUID().toString());
 
+		if (LOG.isLoggable(Level.INFO))	{
+			HttpEntity httpEntity = null;
+
+			if (request instanceof HttpRequestWrapper)  {
+				httpEntity = ((HttpEntityEnclosingRequest) request).getEntity();
+			} else if(request instanceof HttpPost) {
+				httpEntity = ((HttpPost)request).getEntity();
+			}
+
+			if (httpEntity != null) {
+				InputStream content = httpEntity.getContent();
+
+				String responseBody = IOUtils.toString(content, "UTF-8");
+
+				LOG.info(String.format("sendRequest: %s", this.maskSensitiveCardInformations(responseBody)));
+
+				try {
+					content.reset();
+				} catch (Exception e)    {
+					content.mark(0);
+				}
+			}
+		}
+
 		return httpClient.execute(request);
 	}
 
@@ -92,7 +139,73 @@ public abstract class AbstractSaleRequest<Request, Response> {
 			responseBuilder.append(line);
 		}
 
+		if (LOG.isLoggable(Level.INFO))	{
+			LOG.info(String.format("readResponse: %s", this.maskSensitiveCardInformations(responseBuilder.toString())));
+		}
+
 		return parseResponse(response.getStatusLine().getStatusCode(), responseBuilder.toString(), responseClassOf);
+	}
+
+	/**
+	 * <p>
+	 *     Try to mask the sensitive credit card informations, like CVV, validate date and card number
+	 *     If couldn't return a fixed text message
+	 * </p>
+	 * @param value
+	 * @return
+	 */
+	private String maskSensitiveCardInformations(String value) {
+		Matcher matcher = patternCreditCardNumber.matcher(value);
+
+		if (matcher.find()) {
+			String cardNumber = matcher.group(0);
+
+			StringBuilder maskCardNumber = new StringBuilder();
+
+			maskCardNumber.append(cardNumber.substring(0, 6));
+
+			for (int mask = 0; mask < (cardNumber.length() - 10); mask++) {
+				maskCardNumber.append("*");
+			}
+
+			maskCardNumber.append(cardNumber.substring(cardNumber.length() - 4));
+
+			value = value.replace(cardNumber, maskCardNumber.toString());
+
+			matcher = patternValidateDate.matcher(value);
+
+			if (matcher.find()) {
+				value = matcher.replaceAll("\"*\"");
+			}
+
+			matcher = patternSecurityCode.matcher(value);
+
+			if (matcher.find()) {
+				value =  matcher.replaceAll("\"*\"");
+			}
+
+			return value;
+		}	else	{
+			matcher = patternMaskedCreditCardNumber.matcher(value);
+
+			if (matcher.find()) {
+				matcher = patternValidateDate.matcher(value);
+
+				if (matcher.find()) {
+					value = matcher.replaceAll("\"*\"");
+				}
+
+				matcher = patternSecurityCode.matcher(value);
+
+				if (matcher.find()) {
+					value =  matcher.replaceAll("\"*\"");
+				}
+
+				return value;
+			}
+		}
+
+		return "Find a credit card number but can't mask it!";
 	}
 
 	/**
